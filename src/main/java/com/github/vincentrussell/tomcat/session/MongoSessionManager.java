6c90b049461c541aa9e4fd4bb47f45f6ca03bf66
Vincent Russell <vincent.russell@gmail.com>
2020-03-25 14:50:09 -0400

    initial commit

---
+++ src/main/java/com/github/vincentrussell/tomcat/session/MongoSessionManager.java
@@ -0,0 +1,8 @@
+package com.github.vincentrussell.tomcat.session;
+
+import org.apache.catalina.session.PersistentManager;
+import org.apache.catalina.session.PersistentManagerBase;
+
+public class MongoSessionManager extends PersistentManagerBase {
+
+}
